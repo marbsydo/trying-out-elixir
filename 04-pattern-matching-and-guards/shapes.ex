37a1c92b2e3aded23dcd13e4397bdc6ba34db35b
marbsydo <marbsam@yahoo.co.uk>
2015-07-04 18:25:56 +0100

    Pattern matching and guards

---
+++ 04-pattern-matching-and-guards/shapes.ex
@@ -0,0 +1,22 @@
+defmodule Geometry do
+	def area(:rectangle, length, width) when length >= 0 and width >= 0 do
+		length * width
+	end
+
+	def area(:triangle, base, height) when base >= 0 and height >= 0 do
+		0.5 * base * height
+	end
+
+	def area(:circle, radius) when radius >= 0 do
+		3.1415926 * radius * radius
+	end
+
+	def area(:square, side) when side >= 0 do
+		side * side
+	end
+end
+
+IO.puts Geometry.area(:square, 5)
+IO.puts Geometry.area(:circle, 3)
+IO.puts Geometry.area(:triangle, 5, 7)
+IO.puts Geometry.area(:rectangle, 9, 2)
